138d42d5c10ac2e4b45ee1c116078053a68bb43a
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-17 14:56:26 +0300

    UPDATE destroy method

--- app/controllers/concerns/api/v1/articles_controller.rb
+++ app/controllers/concerns/api/v1/articles_controller.rb
@@ -4,47 +4,63 @@ module Api
       def index
         articles = Article.order('created_at DESC')
         render json: {
-                      status: 'Success', 
-                      message: 'Loaded articles', 
-                      data:articles}, status: :ok  
+          status: 'Success',
+          message: 'Loaded articles',
+          data: articles
+        }, status: :ok
       end
 
       def show
         article = Article.find(params[:id])
         render json: {
-                      status: 'Success', 
-                      message: 'Loaded article', 
-                      data:article}, status: :ok  
+          status: 'Success',
+          message: 'Loaded article',
+          data: article
+        }, status: :ok
       end
 
       def create
         article = Article.new(article_params)
         if article.save
           render json: {
-                        status: 'Success', 
-                        message: 'Saved article', 
-                        data:article
-                        }, status: :ok  
-  
+            status: 'Success',
+            message: 'Saved article',
+            data: article
+          }, status: :ok
+
         else
           render json: {
-                        status: 'ERROR', 
-                        message: 'ERROR', 
-                        data:article.errors
-                        }, status: :unprocessable_entity  
-
+            status: 'ERROR',
+            message: 'ERROR',
+            data: article.errors
+          }, status: :unprocessable_entity
 
         end
       end
 
       def destroy
-        article = Article.find(params[:id])
-        if article.destroy
+        begin
+          article = Article.find(params[:id])
+          if article.destroy
+            render json: {
+              status: 'Success',
+              message: "DELETED ENTRY NUMBER #{params[:id]}",
+              data: article
+            }, status: :ok
+          else
+            render json: {
+              status: 'FAILED',
+              message: "FAILED TO DELETE ENTRY NUMBER #{params[:id]}",
+              data: article
+            }, status: :unprocessable_entity
+          end
+        rescue => exception
           render json: {
-            status: 'Success', 
-            message: "DELETED ENTRY NUMBER #{params[:id]}", 
-            data:article
-            }, status: :ok  
+            status: 'FAILED',
+            message: "FAILED TO DELETE ENTRY NUMBER #{params[:id]}",
+            data: ""
+          }, status: :unprocessable_entity
+          return
         end
       end
 
