b60ea0603b579d13087fb1e02fb8d71e2bf71235
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-17 13:02:30 +0300

    ADD articlesController show method

--- app/controllers/concerns/api/v1/articles_controller.rb
+++ app/controllers/concerns/api/v1/articles_controller.rb
@@ -1,11 +1,21 @@
 module Api
-  module v1
+  module V1
     class ArticlesController < ApplicationController
-      articles = Article.order('created_at DESC')
-      render json: {
-        status: 'Success', 
-        message: 'Loaded articles', 
-        data:articles}, status:ok
+      def index
+        articles = Article.order('created_at DESC')
+        render json: {
+          status: 'Success', 
+          message: 'Loaded articles', 
+          data:articles}, status: :ok  
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: {
+          status: 'Success', 
+          message: 'Loaded article', 
+          data:article}, status: :ok  
+      end
     end
   end
 end
